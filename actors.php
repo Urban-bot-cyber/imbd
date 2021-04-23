bce9b8484b8d40d5f60ad3b4f01c618f731083a8
Urban Prosenjak <prosenjak.urban@gmail.com>
2021-04-23 09:21:51 +0200

    Dodajanje slik igralca

--- actors.php
+++ actors.php
@@ -1,32 +1,31 @@
-<?php
+<?php 
 include_once "header.php";
-include_once "db.php"
+include_once "db.php";
 ?>
 
-<a href ="actor_add.php" class="btn btn-primary">Dodaj igralca</a>
-<br/>
+<a href="actor_add.php" class="btn btn-primary">Dodaj igralca</a> 
+<br />
 <div class="actors">
-
 <?php
 $query = "SELECT * FROM actors";
 $stmt = $pdo->prepare($query);
 $stmt->execute();
-//izpis vshe igralcev
-while($row = $stmt-> fetch()){
-    echo '<div class= "actor">';
-    echo $row['first_name'].' '.$row['last_name'];
+//izpis vseh igralcev
+while ($row = $stmt->fetch()) {
+    echo '<div class="actor">';
+    echo '<a href="actor.php?id='.$row['id'].'">';
+        echo $row['first_name'].' '.$row['last_name'];
+    echo '</a>';
     echo '<br />';
     echo '<span>'.$row['nick'].'</span>';
     echo '<br />';
-    echo '<a href="actor_edit.php?id='.$row['id'].'">Uredi</a> ' ;
-    echo '<a href="actor_delete.php?id='.$row['id'].'"onclick="return confirm(\'Prepričani?\')">Izbriši</a> ' ;
+    echo '<a href="actor_edit.php?id='.$row['id'].'">Uredi</a> ';
+    echo '<a href="actor_delete.php?id='.$row['id'].'" onclick="return confirm(\'Prepričani?\')">Izbriši</a> ';
     echo '</div>';
 }
-
 ?>
 </div>
 
-
-<?php
+<?php 
 include_once "footer.php";
 ?>
