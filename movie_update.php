d1a7ad3d2569d5f759b5b94332d2887fb2646191
Urban Prosenjak <prosenjak.urban@gmail.com>
2021-05-07 08:45:08 +0200

    urejanje filmov

---
+++ movie_update.php
@@ -0,0 +1,39 @@
+<?php
+include_once "session.php";
+include_once "db.php";
+
+$id = (int)$_POST['id'];//kateri film urejamo
+$title  = $_POST['title'];
+$duration  = (int)$_POST['duration'];
+$year_release  = (int)$_POST['year_release'];
+$genres = $_POST['genres'];
+$user_id = $_SESSION['user_id'];
+$description = $_POST['description'];
+
+//preverim, če je vnešen naslov
+if((!empty($title))){
+    //posodobim film
+    $query = "UPDATE movies SET title=?,duration=? ,year_release=?,description=? WHERE id=?";
+    $stmt= $pdo->prepare($query);
+    $stmt ->execute([$title,$duration,$year_release,$description,$id]);
+
+    //vse žanre, kateremu film pripada, najprej izbrišemo
+    $query = "DELETE FROM genres_movies WHERE movie_id=?";
+    $stmt= $pdo->prepare($query);
+    $stmt -> execute([$id]);
+    //vnesi žanre
+    foreach($genres as $genre_id){
+        $query = "INSERT INTO genres_movies(genre_id,movie_id)VALUES(?,?)";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$genre_id,$id]);
+    }
+
+
+
+    //preusmeritev na seznam vshe filmov
+    header("Location: movies.php"); die();
+}
+//preusmeritev nazaj na update - nekaj je narobe
+header("Location: movie_edit.php?id=$id"); die();
+
+?>
